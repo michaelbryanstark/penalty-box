a14986c72c954d774de2c7bf739363b4c65827ae
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2021-11-13 13:08:47 -0500

    added crud to posts

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    path('posts/<int:pk>/edit', views.PostEdit.as_view(), name="post_edit"),
+    path('posts/<int:pk>/delete', views.PostDelete.as_view(), name="post_delete"),
+    path('posts/new/', views.PostCreate.as_view(), name="post_create"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views import generic
 from .models import Post
+from django.views.generic.edit import DeleteView, CreateView, UpdateView
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -9,4 +10,28 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
-# Create your views here.
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = '/'
+
+class PostEdit(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post_edit.html'
+    def get_success_url(self):
+         print(self.kwargs)
+         return reverse('post_detail', kwargs={'pk': self.object.pk})
+     
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'content', 'author', 'status']
+    template_name = "post_create.html"
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(PostCreate, self).form_valid(form)
+    
+    def get_success_url(self):
+        print(self.kwargs)
+        return reverse('post_detail', kwargs={'pk': self.object.pk})
